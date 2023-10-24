a59492697c20c2e18568d6086328277b0de7de5b
Triggered831 <60940169+Triggered831@users.noreply.github.com>
2023-10-24 13:31:04 +0300

    Add files via upload

---
+++ mass1/mass1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+Console.WriteLine("Введите числа");
+int[] ints = Console.ReadLine().Split().Select(int.Parse).ToArray();
+Console.Write("Вставить после: ");
+int k = Convert.ToInt32(Console.ReadLine());
+int[] ints2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int num = ints.Length - 1;
+int[] ints3 = new int[ints.Length + ints2.Length];
+ints3 = ints.SkipLast(ints.Length-k).ToArray();
+ints3 = ints3.Concat(ints2).ToArray();
+ints3 = ints3.Concat(ints.Skip(k)).ToArray();
+foreach (int i in ints3)
+    Console.Write(i+" ");
